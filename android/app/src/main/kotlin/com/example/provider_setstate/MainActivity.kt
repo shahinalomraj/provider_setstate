c45cc8c108dceaededf6a19226d1fcbcb7110ded
Shahin Alom <shahinalomraj93@gmail.com>
2022-04-03 16:43:36 +0600

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/provider_setstate/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.provider_setstate
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+class _HomePageState extends State<HomePage> {
+  int value=0;
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(value.toString(),style: TextStyle(fontSize: 50),)
+          ]
+        ),
+      ),
+    );
+  }
+}
